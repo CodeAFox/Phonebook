e8b58e7f55efb548ff78ae29b24ad48a23b60072
CodeAFox <illeskrisztinacsilla@gmail.com>
2025-07-18 19:25:23 +0200

    Person entity created

--- Phonebook/src/main/java/Entities/Person.java
+++ Phonebook/src/main/java/Entities/Person.java
@@ -2,4 +2,46 @@ package Entities;
 
 public class Person
 {
+  private String name;
+  private int age;
+  private int Id;
+
+  public Person(){}
+
+  public Person(String name, int age, int Id)
+  {
+    this.name = name;
+    this.age = age;
+    this.Id = Id;
+  }
+
+  public void setName(String name)
+  {
+    this.name = name;
+  }
+
+  public void setAge(int age)
+  {
+    this.age = age;
+  }
+
+  public void setId(int Id)
+  {
+    this.Id = Id;
+  }
+
+  public String getName()
+  {
+    return name;
+  }
+
+  public int getAge()
+  {
+    return age;
+  }
+
+  public int getId()
+  {
+    return Id;
+  }
 }
