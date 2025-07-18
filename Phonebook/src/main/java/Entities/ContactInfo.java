e9c56d267d833e28baadeb9d0159421f563f8f68
CodeAFox <illeskrisztinacsilla@gmail.com>
2025-07-18 21:44:17 +0200

    ContactInfo entity created

--- Phonebook/src/main/java/Entities/ContactInfo.java
+++ Phonebook/src/main/java/Entities/ContactInfo.java
@@ -2,6 +2,36 @@ package Entities;
 
 public class ContactInfo
 {
+  private String type;
+  private String contact;
+
+  public ContactInfo(){}
+
+  public ContactInfo(String type, String contact)
+  {
+    this.type = type;
+    this.contact = contact;
+  }
+
+  public void setType(String type)
+  {
+    this.type = type;
+  }
+
+  public void setContact(String contact)
+  {
+    this.contact = contact;
+  }
+
+  public String getType()
+  {
+    return type;
+  }
+
+  public String getContact()
+  {
+    return contact;
+  }
 
   public boolean equals(Object obj)
   {
