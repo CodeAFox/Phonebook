95348e8e76aa1e26afed1e12b3d48367a604340d
CodeAFox <illeskrisztinacsilla@gmail.com>
2025-07-18 20:11:51 +0200

    Address entity created

--- Phonebook/src/main/java/Entities/Address.java
+++ Phonebook/src/main/java/Entities/Address.java
@@ -1,5 +1,51 @@
 package Entities;
 
+import java.util.ArrayList;
+import java.util.Objects;
+
 public class Address
 {
+  private String address;
+  private ArrayList<ContactInfo> contacts = new ArrayList<>();
+
+  public Address()
+  {
+  }
+
+  public Address(String address, ArrayList<ContactInfo> contacts)
+  {
+    this.address = address;
+    this.contacts = new ArrayList<>(contacts);
+  }
+
+  public void setAddress(String address)
+  {
+    this.address = address;
+  }
+
+  public void setContacts(ArrayList<ContactInfo> contacts)
+  {
+    this.contacts = new ArrayList<>(contacts);
+  }
+
+  public void addContact(ContactInfo contact)
+  {
+    contacts.add(contact);
+  }
+
+  public String getAddress()
+  {
+    return address;
+  }
+
+  public ArrayList<ContactInfo> getContacts()
+  {
+    return new ArrayList<>(contacts);
+  }
+
+  public ContactInfo removeContact(ContactInfo contact)
+  {
+    return contacts.remove(contacts.indexOf(contact));
+  }
+
 }
