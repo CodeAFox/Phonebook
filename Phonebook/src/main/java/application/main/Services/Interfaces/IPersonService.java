bc25b886775ef8dcecde87f1561e6af896ed24f6
CodeAFox <illeskrisztinacsilla@gmail.com>
2025-07-20 16:20:07 +0200

    IPersonService adjusted to fit PersonController

--- Phonebook/src/main/java/application/main/Services/Interfaces/IPersonService.java
+++ Phonebook/src/main/java/application/main/Services/Interfaces/IPersonService.java
@@ -6,9 +6,9 @@ import java.util.ArrayList;
 
 public interface IPersonService
 {
-  public Person addPerson();
+  public Person createPerson(Person person);
   public Person getPerson(int Id);
   public ArrayList<Person> getAllPeople();
-  public void updatePerson(Person person);
+  public Person updatePerson(Person person);
   public Person deletePerson(int Id);
 }
