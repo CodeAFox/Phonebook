c3e102202a72a314736ff358162e94a22317ebe3
CodeAFox <illeskrisztinacsilla@gmail.com>
2025-07-20 16:16:13 +0200

    PersonController with basic error handling created

--- Phonebook/src/main/java/application/main/Controllers/PersonController.java
+++ Phonebook/src/main/java/application/main/Controllers/PersonController.java
@@ -2,26 +2,90 @@ package application.main.Controllers;
 
 import application.main.Entities.Address;
 import application.main.Entities.Person;
+import application.main.Services.Interfaces.IPersonService;
+import application.main.Services.PersonService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/api/people")
 public class PersonController
 {
-  ObjectMapper mapper = new ObjectMapper();
-  @GetMapping("/{id}")
-  public String getPerson(@PathVariable("id") int id)
+  IPersonService personService = new PersonService();
+  @PostMapping("/")
+  public ResponseEntity<Person> createPerson(@RequestBody Person person)
   {
-    Person temp = new Person("Kate", 32, id, new Address("somewhere"));
     try
     {
-      return mapper.writeValueAsString(temp);
+      personService.createPerson(person);
     }
-    catch (JsonProcessingException e)
+    //Placeholder, perhaps specify later on
+    catch (Exception e)
     {
-      return e.getMessage();
+      return new ResponseEntity<>(null, HttpStatus.CONFLICT);
+    }
+    return new ResponseEntity<>(person, HttpStatus.CREATED);
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<Person> getPerson(@PathVariable("id") int id)
+  {
+    try
+    {
+      Person person = personService.getPerson(id);
+      return new ResponseEntity<>(person, HttpStatus.OK);
+    }
+    //Need more specific error handling for when person under id cannot be found
+    catch (Exception e)
+    {
+      return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  @GetMapping("/")
+  public ResponseEntity<ArrayList<Person>> getAllPeople()
+  {
+    try
+    {
+      ArrayList<Person> allPeople = personService.getAllPeople();
+      return new ResponseEntity<>(allPeople, HttpStatus.OK);
+    }
+    catch (Exception e)
+    {
+      return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  @PutMapping("/")
+  public ResponseEntity<Person> updatePerson(@RequestBody Person person)
+  {
+    try
+    {
+      Person updated = personService.updatePerson(person);
+      return new ResponseEntity<>(updated, HttpStatus.OK);
+    }
+    catch (Exception e)
+    {
+      return new ResponseEntity<>(null, HttpStatus.NOT_MODIFIED);
+    }
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Person> deletePerson(@PathVariable("id") int id)
+  {
+    try
+    {
+      Person deleted = personService.deletePerson(id);
+      return new ResponseEntity<>(deleted, HttpStatus.OK);
+    }
+    catch (Exception e)
+    {
+      return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
     }
   }
 }
